85b1330fbfc6e1e22885e9d22a6f29709e72f6a1
Ervin Csala <csala.ervin@gmail.com>
2019-03-26 21:37:05 +0100

    plusBucketList

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var shoppingList = ['bread','milk','apple'];
+shoppingList.push('eggs');
+shoppingList.push('rice','pasta','beans');
+shoppingList.unshift('chocolate');
+shoppingList.unshift('tea');
+shoppingList.shift();
+shoppingList.push('tea');
+shoppingList.pop();
+alert(shoppingList);
+var houses = ['houseArryn','houseMormont','houseGreyjoy','houseClegane','houseLannister','houseStark','houseTyrell','houseTargaryen','houseBaratheon','houseTully']
+alert(houses);
+var fibonacci = [0,1,1,2,3,5,8,13,21,34];
+var deletedFromEnd = fibonacci.pop();
+alert(fibonacci);
+
+var bucketList =['writing a novel','learn Italian','travel to New Zealand','diving'];
+bucketList.push('seeing the polar light','learn to play guitar','parachuting');
+bucketList.unshift('to become a vegetarian','move to France for about 3 months');
+bucketList.shift();
+console.log(bucketList);
