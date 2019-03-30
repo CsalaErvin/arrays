fe23fff6c7e98cb56f6c5e4c656d646dcc0ce027
Ervin Csala <csala.ervin@gmail.com>
2019-03-31 00:02:39 +0100

    guestListAdded

--- arrays.js
+++ arrays.js
@@ -7,14 +7,25 @@ shoppingList.shift();
 shoppingList.push('tea');
 shoppingList.pop();
 alert(shoppingList);
+
 var houses = ['houseArryn','houseMormont','houseGreyjoy','houseClegane','houseLannister','houseStark','houseTyrell','houseTargaryen','houseBaratheon','houseTully']
 alert(houses);
+
 var fibonacci = [0,1,1,2,3,5,8,13,21,34];
 var deletedFromEnd = fibonacci.pop();
 alert(fibonacci);
 
-var bucketList =['writing a novel','learn Italian','travel to New Zealand','diving'];
+var bucketList = ['writing a novel','learn Italian','travel to New Zealand','diving'];
 bucketList.push('seeing the polar light','learn to play guitar','parachuting');
 bucketList.unshift('to become a vegetarian','move to France for about 3 months');
 bucketList.shift();
 console.log(bucketList);
+alert(shoppingList[0]);
+shoppingList[0] = 'candy';
+alert(shoppingList[0]);
+
+var guestList = ['Tomi','Lili','Balázs','Zsuzsi','Misi','Kati','Janka','Pali'];
+alert(guestList);
+guestList[5] = 'Marcsi';
+alert(guestList);
+								 
