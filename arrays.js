e001b06570a7621032a2721b2e8c9f8b9064c3b6
Ervin Csala <csala.ervin@gmail.com>
2019-04-05 06:07:29 +0200

    xVersion

--- arrays.js
+++ arrays.js
@@ -1,30 +1,39 @@
-var shoppingList = ['bread','milk','apple'];
+/*jslint devel: true */
+
+var shoppingList = ['bread', 'milk', 'apple'];
 shoppingList.push('eggs');
-shoppingList.push('rice','pasta','beans');
+shoppingList.push('rice', 'pasta', 'beans');
 shoppingList.unshift('chocolate');
 shoppingList.unshift('tea');
 shoppingList.shift();
 shoppingList.push('tea');
 shoppingList.pop();
 alert(shoppingList);
+shoppingList.splice(2, 0, 'chease');
 
-var houses = ['houseArryn','houseMormont','houseGreyjoy','houseClegane','houseLannister','houseStark','houseTyrell','houseTargaryen','houseBaratheon','houseTully']
-alert(houses);
-
-var fibonacci = [0,1,1,2,3,5,8,13,21,34];
-var deletedFromEnd = fibonacci.pop();
-alert(fibonacci);
-
-var bucketList = ['writing a novel','learn Italian','travel to New Zealand','diving'];
-bucketList.push('seeing the polar light','learn to play guitar','parachuting');
-bucketList.unshift('to become a vegetarian','move to France for about 3 months');
-bucketList.shift();
-console.log(bucketList);
 alert(shoppingList[0]);
 shoppingList[0] = 'candy';
 alert(shoppingList[0]);
 
-var guestList = ['Tomi','Lili','Balázs','Zsuzsi','Misi','Kati','Janka','Pali'];
+shoppingList.splice(4, 1);
+shoppingList.splice(4, 3);
+alert(shoppingList);
+
+var houses = ['houseArryn', 'houseMormont', 'houseGreyjoy', 'houseClegane', 'houseLannister', 'houseStark', 'houseTyrell', 'houseTargaryen', 'houseBaratheon', 'houseTully'];
+alert(houses);
+
+var fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+var deletedFromEnd = fibonacci.pop();
+alert(fibonacci);
+
+var bucketList = ['writing a novel', 'learn Italian', 'travel to New Zealand', 'diving'];
+bucketList.push('seeing the polar light', 'learn to play guitar', 'parachuting');
+bucketList.unshift('to become a vegetarian', 'move to France for about 3 months');
+bucketList.shift();
+alert(bucketList);
+
+
+var guestList = ['Tomi', 'Lili', 'Balázs', 'Zsuzsi', 'Misi', 'Kati', 'Janka', 'Pali'];
 alert(guestList);
 guestList[5] = 'Marcsi';
 alert(guestList);
